ca93c26c112af935c353aba086e214cd32d7d8f8
Jason Li <haotian-li@outlook.com>
2022-01-07 16:44:31 -0800

    introduce Model class and build User class by inheriting from Model

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "Claire", id: 1 });
-
+const user = User.buildUser({ name: "Jay", age: 44, id: 1 });
 user.on("change", () => {
-  console.log("change event!! we might need to change some HTML");
-	console.log(user);
+  console.log("change");
+  console.log(user);
 });
-user.on('save', () => {
-	console.log(user);
-})
-// user.set({name: 'new name', id: 1})
-// user.fetch();
-user.save();
+user.get("id");
+user.fetch();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
 import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { APISync } from "./APISync";
 import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
+import { Model } from "./Model";
 
 export interface UserProps {
   id?: number;
@@ -11,53 +11,17 @@ export interface UserProps {
 
 const rootURL = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootURL);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    // here is calling constructor of parent class Model
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new APISync<UserProps>(rootURL)
+    );
   }
 
-  // on(eventName: string, callback: Callback): void {
-  //     this.events.on(eventName, callback);
-  //   }
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.attributes.get("id");
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without an id");
-    }
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data); // use this.set as we also want to trigger 'change' event
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger("save");
-      })
-      .catch(() => {
-        this.trigger("error");
-      });
+  public isAdminUser(): boolean {
+    return this.get('id') === 1;
   }
 }
